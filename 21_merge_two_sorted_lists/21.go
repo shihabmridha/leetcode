182c8ee7db66877d76c48fa82f4dcef830a24de0
Shihab Mridha <shihabmridha@gmail.com>
2023-05-10 22:34:30 +0600

    solve: 21 - Merge two sorted lists

---
+++ 21_merge_two_sorted_lists/21.go
@@ -0,0 +1,41 @@
+package solution
+
+import "sort"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	list := []int{}
+
+	for list1 != nil {
+		list = append(list, list1.Val)
+		list1 = list1.Next
+	}
+
+	for list2 != nil {
+		list = append(list, list2.Val)
+		list2 = list2.Next
+	}
+
+	sort.Slice(list, func(i, j int) bool {
+		return list[i] < list[j]
+	})
+
+	var head *ListNode = nil
+	var result *ListNode = nil
+
+	for _, e := range list {
+		if result == nil {
+			head = &ListNode{Val: e}
+			result = head
+		} else {
+			result.Next = &ListNode{Val: e}
+			result = result.Next
+		}
+	}
+
+	return head
+}
