b7a2757220a7b7a44a9b7e3f18ed8c8f349e624c
Shihab Mridha <shihabmridha@gmail.com>
2023-09-24 07:23:24 +0600

    wip

--- 153_find_minimum_in_rotated_sorted_array/153.go
+++ 153_find_minimum_in_rotated_sorted_array/153.go
@@ -1,5 +1,23 @@
 package findminimuminrotatedsortedarray
 
 func findMin(nums []int) int {
+	ln := len(nums)
 
+	low := 0
+	high := ln
+	mid := low + ((high - low) / 2)
+
+	for low < high {
+		curr := nums[mid]
+		next := nums[mid+1]
+		prev := nums[mid-1]
+
+		if curr < next {
+
+		}
+
+		// if nums[mid]
+	}
+
+	return 0
 }
--- 153_find_minimum_in_rotated_sorted_array/153_test.go
+++ 153_find_minimum_in_rotated_sorted_array/153_test.go
@@ -4,9 +4,20 @@ import "testing"
 
 func TestFindMin(t *testing.T) {
 	res := findMin([]int{3, 4, 5, 1, 2})
-
 	if res != 1 {
 		t.Logf("Output: %d, Expected: 1", res)
+		t.Fail()
+	}
+
+	res = findMin([]int{4, 5, 6, 7, 0, 1, 2})
+	if res != 0 {
+		t.Logf("Output: %d, Expected: 0", res)
+		t.Fail()
+	}
+
+	res = findMin([]int{11, 13, 15, 17})
+	if res != 11 {
+		t.Logf("Output: %d, Expected: 11", res)
 		t.Fail()
 	}
 }
